704b6ae5f5adc5fa729a657fb18381636f91aef4
Mohan <v.mohanraj2005@gmail.com>
2026-02-28 11:15:08 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use(cors({
   origin: [
     "http://localhost:5173",
     "http://localhost:5174",
-    "https://majesty-seven.vercel.app"
+    "https://majesty-rose.vercel.app/"
   ],
   credentials: true
 }));
